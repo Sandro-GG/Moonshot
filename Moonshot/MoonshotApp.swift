74dadb2714db17dcbc3adeabb9b1fdf13da48cc0
Sandro Gakharia <sandrogakharia@gmail.com>
2025-07-22 16:08:31 +0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Sandro Gakharia on 22.07.25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoonshotTests/MoonshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotTests.swift
+//  MoonshotTests
+//
+//  Created by Sandro Gakharia on 22.07.25.
+//
+
+import Testing
+@testable import Moonshot
+
+struct MoonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
